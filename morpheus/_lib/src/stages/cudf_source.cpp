ea70ee556921f92580b86b16078df697a8be77e4
David Gardner <dagardner@nvidia.com>
2024-05-22 16:53:03 -0700

    Remove redundant sync

--- morpheus/_lib/src/stages/cudf_source.cpp
+++ morpheus/_lib/src/stages/cudf_source.cpp
@@ -87,8 +87,7 @@ CudfSourceStage::subscriber_fn_t CudfSourceStage::build()
             auto float_buffer_copy =
                 rmm::device_uvector<float>(*float_buffer, float_buffer->stream(), float_buffer->memory_resource());
 
-            mrc::enqueue_stream_sync_event(int_buffer_copy.stream()).get();
-            mrc::enqueue_stream_sync_event(float_buffer_copy.stream()).get();
+            mrc::enqueue_stream_sync_event(rmm::cuda_stream_per_thread).get();
 
             std::vector<std::unique_ptr<cudf::column>> columns;
 
