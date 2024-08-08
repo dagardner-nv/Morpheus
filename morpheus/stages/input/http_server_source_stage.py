fab2159a3137e45188fec1897b1f12ea3e5990f9
David Gardner <dagardner@nvidia.com>
2024-08-08 16:04:44 -0700

    Stop the http server on shutdown

--- morpheus/stages/input/http_server_source_stage.py
+++ morpheus/stages/input/http_server_source_stage.py
@@ -171,6 +171,8 @@ class HttpServerSourceStage(PreallocatorMixin, ConfigurableOutputSource):
         logger.debug("Stopping HttpServerSourceStage")
         # Indicate we need to stop
         self._stop_requested = True
+        if self._http_server is not None:
+            self._http_server.stop()
 
         return super().stop()
 
