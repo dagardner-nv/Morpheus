d3530732787cb2941816f1f902482d5e16835f61
David Gardner <dagardner@nvidia.com>
2024-09-11 09:00:43 -0700

    Repro scripts

---
+++ cf.py
@@ -0,0 +1,134 @@
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import typing
+
+import mrc
+from mrc.core import operators as ops
+
+from dask.distributed import Client
+
+from common.data_models import FeatureConfig  # pylint: disable=no-name-in-module
+from common.feature_extractor import FeatureExtractor  # pylint: disable=no-name-in-module
+from morpheus.config import Config
+from morpheus.messages import ControlMessage
+from morpheus.pipeline.single_port_stage import SinglePortStage
+from morpheus.pipeline.stage_schema import StageSchema
+from morpheus.stages.input.appshield_source_stage import AppShieldMessageMeta
+
+
+class ModifiedCreateFeaturesRWStage(SinglePortStage):
+    """
+    This class extends MultiMessageStage to deal with scenario specific features from Appshiled plugins data.
+
+    Parameters
+    ----------
+    c : morpheus.config.Config
+        Pipeline configuration instance
+    interested_plugins : typing.List[str]
+        Only intrested plugins files will be read from Appshield snapshots
+    feature_columns : typing.List[str]
+        List of features needed to be extracted.
+    file_extns : typing.List[str]
+        File extensions.
+    n_workers: int, default = 2
+        Number of dask workers.
+    threads_per_worker: int, default = 2
+        Number of threads for each dask worker.
+    """
+
+    def __init__(
+        self,
+        c: Config,
+        interested_plugins: typing.List[str],
+        feature_columns: typing.List[str],
+        file_extns: typing.List[str],
+        n_workers: int = 2,
+        threads_per_worker: int = 2,
+    ):
+        self._client = Client(threads_per_worker=threads_per_worker, n_workers=n_workers)
+        self._feature_config = FeatureConfig(file_extns, interested_plugins)
+        self._feas_all_zeros = dict.fromkeys(feature_columns, 0)
+
+        # FeatureExtractor instance to extract features from the snapshots.
+        self._fe = FeatureExtractor(self._feature_config)
+
+        super().__init__(c)
+
+    @property
+    def name(self) -> str:
+        return "create-features-rw"
+
+    def accepted_types(self) -> typing.Tuple:
+        """
+        Returns accepted input types for this stage.
+        """
+        return (AppShieldMessageMeta, )
+
+    def supports_cpp_node(self):
+        return False
+
+    def compute_schema(self, schema: StageSchema):
+        schema.output_schema.set_type(ControlMessage)
+
+    def on_next(self, x: AppShieldMessageMeta):
+
+        snapshot_fea_dfs = []
+
+        df = x.df
+
+        # Type cast CommitCharge.
+        df["CommitCharge"] = df["CommitCharge"].astype("float").astype("Int32")
+        df["Name"] = df["Name"].str.lower()
+
+        # Create PID_Process feature.
+        df['PID_Process'] = df.PID + '_' + df.Process
+
+        snapshot_ids = df.snapshot_id.unique()
+
+        if len(snapshot_ids) > 1:
+            # Group snapshot rows using snapshot id.
+            all_dfs = [df[df.snapshot_id == snapshot_id] for snapshot_id in snapshot_ids]
+        else:
+            all_dfs = [df]
+
+        extract_func = self._fe.extract_features
+        combine_func = FeatureExtractor.combine_features
+
+        # Schedule dask task `extract_features` per snapshot.
+        snapshot_fea_dfs = self._client.map(extract_func, all_dfs, feas_all_zeros=self._feas_all_zeros)
+
+        # Combined `extract_features` results.
+        features_df = self._client.submit(combine_func, snapshot_fea_dfs)
+
+        # Gather features from all the snapshots.
+        features_df = features_df.result()
+
+        # Snapshot sequence will be generated using `source_pid_process`.
+        # Determines which source generated the snapshot messages.
+        # There's a chance of receiving the same snapshots names from multiple sources(hosts)
+        features_df['source_pid_process'] = x.source + '_' + features_df.pid_process
+
+        # Sort entries by pid_process and snapshot_id
+        features_df = features_df.sort_values(by=["pid_process", "snapshot_id"]).reset_index(drop=True)
+
+    def on_completed(self):
+        # Close dask client when pipeline initiates shutdown
+        self._client.close()
+
+    def _build_single(self, builder: mrc.Builder, input_node: mrc.SegmentObject) -> mrc.SegmentObject:
+        node = builder.make_node(self.unique_name, ops.map(self.on_next), ops.on_completed(self.on_completed))
+        builder.make_edge(input_node, node)
+
+        return node
