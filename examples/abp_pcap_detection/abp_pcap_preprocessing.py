d7313ef1a367ae0ba976e3d87a7c95653b38e5fb
David Gardner <dagardner@nvidia.com>
2025-01-23 12:15:37 -0800

    Avoid multiple calls to set_data

--- examples/abp_pcap_detection/abp_pcap_preprocessing.py
+++ examples/abp_pcap_detection/abp_pcap_preprocessing.py
@@ -175,8 +175,7 @@ class AbpPcapPreprocessingStage(PreprocessBaseStage):
         data = cp.asarray(merged_df[fea_cols].to_cupy(), order='C', dtype=cp.float32)
         count = data.shape[0]
 
-        for col in req_cols:
-            meta.set_data(col, merged_df[col])
+        meta.set_data(req_cols, merged_df[req_cols])
 
         del merged_df
 
