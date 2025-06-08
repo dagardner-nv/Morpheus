039cc2689fd7f08b63f75b5317a9b7b559cb70d0
David Gardner <dagardner@nvidia.com>
2025-06-07 17:58:39 -0700

    WIP
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- ci/conda/recipes/morpheus-libs/morpheus_dfp_test.sh
+++ ci/conda/recipes/morpheus-libs/morpheus_dfp_test.sh
@@ -22,6 +22,10 @@ fi
 package_name="morpheus_dfp"
 file_name="morpheus_dfp_arch-$(arch).yaml"
 
+
+echo "************************************************"
+echo "Installing test dependencies for ${package_name} using ${file_name}"
+
 # Install requirements if they are included in the package
 python3 <<EOF
 import subprocess
@@ -34,5 +38,8 @@ file_name = "${file_name}"
 with ir.as_file(ir.files(anchor=package_name).joinpath(file_name)) as requirements_file:
     subprocess.call(f"conda env update --solver=libmamba -f {requirements_file}".split())
 EOF
+
+echo "done"
+echo "************************************************"
 
 pytest tests/${package_name}
