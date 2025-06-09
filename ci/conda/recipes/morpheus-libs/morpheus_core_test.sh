fc4d099a38f552f1a2ab7b13d82bb9f5162bed0e
David Gardner <dagardner@nvidia.com>
2025-06-09 11:12:32 -0700

    WIP
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- ci/conda/recipes/morpheus-libs/morpheus_core_test.sh
+++ ci/conda/recipes/morpheus-libs/morpheus_core_test.sh
@@ -19,20 +19,4 @@ if [[ $(arch) == "aarch64" ]]; then
     exit 0
 fi
 
-package_name="morpheus"
-file_name="requirements_morpheus_core_arch-$(arch).txt"
-
-# Install requirements if they are included in the package
-python3 <<EOF
-import subprocess
-import sys
-import importlib.resources as ir
-
-package_name = "${package_name}"
-file_name = "${file_name}"
-
-with ir.as_file(ir.files(anchor=package_name).joinpath(file_name)) as requirements_file:
-    subprocess.call(f"pip install -r {requirements_file}".split())
-EOF
-
 pytest tests/${package_name}
